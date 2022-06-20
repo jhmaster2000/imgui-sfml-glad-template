e22c572d581719a936c69a13313006a2dcb8042b
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-06-19 23:57:41 -0300

    animSpeed

--- include/actors/firebar.h
+++ include/actors/firebar.h
@@ -3,7 +3,7 @@
 
 class FireBar : public RectActor {
 public:
-    FireBar(unsigned short numFireballs, unsigned short numBars = 1, float speed = 1.0f, float scale = 50.0f, bool anim = false) : RectActor(sf::Vector2f(scale, scale)) {
+    FireBar(unsigned short numFireballs, unsigned short numBars = 1, float speed = 1.0f, float scale = 50.0f, bool anim = false, short animSpeed = 200) : RectActor(sf::Vector2f(scale, scale)) {
         this->setFillColor(sf::Color(0x999999FF));
         for (int i = 0; i < numFireballs * numBars; i++) {
             this->fireballs.push_back(new CircleActor((scale / 5) * 4, 8));
@@ -17,6 +17,7 @@ public:
         this->radius = scale + (scale / 5);
         this->speed = speed;
         this->anim = anim;
+        this->animSpeed = animSpeed;
     }
     virtual ~FireBar() {
         for (int i = 0; i < this->fireballs.size(); i++) delete this->fireballs[i];
@@ -25,16 +26,16 @@ public:
     void onExecute() override {
         if (this->anim) {
             this->timerGoingUp ? this->timer++: this->timer--;
-            if (this->timer == 30) this->timerGoingUp = false;
-            if (this->timer == -30) this->timerGoingUp = true;
+            if (this->timer == this->animSpeed) this->timerGoingUp = false;
+            if (this->timer == -this->animSpeed) this->timerGoingUp = true;
         } else {
-            this->timer = this->speed < 0 ? -30 : 30;
+            this->timer = this->speed < 0 ? -this->animSpeed : this->animSpeed;
         }
         this->angle += this->speed;
         for (int b = 0; b < this->numBars; b++) {
             for (int f = 0; f < this->numFireballs; f++) {
                 const int i = f + b * this->numFireballs;
-                const int angle = (this->angle - (f * 5) * (this->timer / 30.0f)) + (360 / this->numBars * b);
+                const int angle = (this->angle - ((float)f * 5.0f) * ((float)this->timer / (float)this->animSpeed)) + (360.0f / this->numBars * b);
                 this->fireballs[i]->orbit(this->xy(), this->radius * (f + 1), angle);
                 this->fireballs[i]->rotate(-5.0f);
             }
@@ -45,9 +46,10 @@ public:
         BaseActor::onDraw(window);
     }
 
-    float speed;
-    float angle;
     float radius;
+    float angle;
+    float speed;
+    float animSpeed;
     bool anim;
     bool timerGoingUp;
     short timer;
